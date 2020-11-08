ef53f38ef58ee372b6cf0385566b4a24695f1ca7
locke duan <locke@lockes-MacBook-Pro.local>
2020-11-08 18:44:35 +0800

    init

---
+++ TestDotNetDocker/Controllers/WeatherForecastController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace TestDotNetDocker.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            var rng = new Random();
+            var env = Environment.GetEnvironmentVariable("env");
+            return env;
+        }
+
+    }
+}
---
+++ TestDotNetDocker/README.md
@@ -0,0 +1,42 @@
+﻿
+
+FROM mcr.microsoft.com/dotnet/core/sdk:3.1 AS build-env
+WORKDIR /app
+
+# Copy csproj and restore as distinct layers
+COPY *.csproj ./
+RUN dotnet restore
+
+# Copy everything else and build
+COPY . ./
+RUN dotnet publish -c Release -o out
+
+# Build runtime image
+FROM mcr.microsoft.com/dotnet/core/aspnet:3.1
+WORKDIR /app
+COPY --from=build-env /app/out .
+ENTRYPOINT ["dotnet", "aspnetapp.dll"]
+
+
+------------------------------------------------------
+
+FROM mcr.microsoft.com/dotnet/core/sdk:3.1 AS build
+COPY . ./app
+WORKDIR ./app
+RUN dotnet build -c Release -o output
+
+
+FROM mcr.microsoft.com/dotnet/core/aspnet:3.1 AS runtime
+WORKDIR ./app
+EXPOSE 5001
+COPY --from=build /app/output .
+ENTRYPOINT ["dotnet", "TestDotNetDocker.dll"]
+
+
+# How to run
+docker build -t test-dotnet-docker .
+docker run -p 5001:80  -e env=prodution test-dotnet-docker
+
+Or
+
+docker-compose up
